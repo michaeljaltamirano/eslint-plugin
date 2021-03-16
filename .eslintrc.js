d1d9bc1562b24ac1ce99867f460ad6c9afa8f7a7
michaeljaltamirano <michaeljaltamirano@gmail.com>
2021-03-16 11:25:02 -0700

    Update project eslint config to reflect eslint-plugin concerns

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
 	root: true,
 	env: {
-		browser: true,
+		browser: false,
 		es2021: true,
 		node: true,
 	},
 	extends: [
 		'eslint:recommended',
-		'plugin:react/recommended',
 		'plugin:@typescript-eslint/recommended',
-		'airbnb',
+		'airbnb-base',
 		'plugin:prettier/recommended',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
 		ecmaVersion: 12,
 		sourceType: 'module',
 	},
-	plugins: ['react', '@typescript-eslint'],
 	rules: {
 		'no-tabs': 'off',
 		indent: ['error', 'tab'],
