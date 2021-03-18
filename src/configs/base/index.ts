2abff953aac89db8b5f47dcb5b9dfc7384a2ea13
michaeljaltamirano <michaeljaltamirano@gmail.com>
2021-03-17 18:22:33 -0700

    Add eslint-plugin-promise rules to base

--- src/configs/base/index.ts
+++ src/configs/base/index.ts
@@ -1,3 +1,10 @@
 export const base = {
-	extends: ['eslint:recommended', 'airbnb-base'],
+	extends: ['eslint:recommended', 'plugin:promise/recommended', 'airbnb-base'],
+	rules: {
+		'promise/no-nesting': 'error',
+		'promise/no-return-in-finally': 'error',
+		'promise/valid-params': 'error', // Not needed if using TS
+		'promise/prefer-await-to-then': 'error',
+		'promise/prefer-await-to-callbacks': 'error',
+	},
 } as const;
